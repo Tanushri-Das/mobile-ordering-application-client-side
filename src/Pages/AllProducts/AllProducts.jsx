05993e3551cce283609c7c08bf50ace78ccb5d20
tanu <dastanushri402@gmail.com>
2023-12-22 11:04:45 +0600

    add search functionality client side code

--- src/Pages/AllProducts/AllProducts.jsx
+++ src/Pages/AllProducts/AllProducts.jsx
@@ -1,43 +1,12 @@
-// import React, { useEffect, useState } from "react";
-// import Products from "../Products/Products";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-// const AllProducts = () => {
-//   const [allProducts, setAllProducts] = useState([]);
-
-//   useEffect(() => {
-//     fetch("http://localhost:5000/products")
-//       .then((res) => res.json())
-//       .then((data) => setAllProducts(data));
-//   }, []);
-
-//   return (
-//     <div className="my-12">
-//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mx-20">
-//         {allProducts.map((product) => (
-//           <Products key={product._id} product={product} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AllProducts;
-
-
-
-
-
-
-
-
-
-// AllProducts.jsx
-// AllProducts.jsx
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import Products from '../Products/Products';
-import { selectAllProducts, selectFilteredProducts, setAllProducts } from '../../redux/productsSlice';
+import Products from "../Products/Products";
+import {
+  selectAllProducts,
+  selectFilteredProducts,
+  setAllProducts,
+} from "../../redux/productsSlice";
 
 const AllProducts = () => {
   const dispatch = useDispatch();
@@ -47,19 +16,22 @@ const AllProducts = () => {
   useEffect(() => {
     // Fetch all products only if allProducts is empty
     if (allProducts.length === 0) {
-      fetch('http://localhost:5000/products')
+      fetch("http://localhost:5000/products")
         .then((res) => res.json())
         .then((data) => dispatch(setAllProducts(data)))
-        .catch((error) => console.error('Error fetching all products:', error));
+        .catch((error) => console.error("Error fetching all products:", error));
     }
   }, [allProducts, dispatch]);
 
-  const displayProducts = filteredProducts.length > 0 ? filteredProducts : allProducts;
+  const displayProducts =
+    filteredProducts.length > 0 ? filteredProducts : allProducts;
 
   return (
     <div className="my-12">
-      <h3 className="text-2xl text-center mb-6 font-bold">{filteredProducts.length > 0 ? 'Searched Products' : 'All Products'}</h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-20">
+      <h3 className="text-4xl text-center mb-12 font-bold">
+        {filteredProducts.length > 0 ? "Searched Products" : "All Products"}
+      </h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-20">
         {displayProducts.map((product) => (
           <Products key={product._id} product={product} />
         ))}
@@ -69,4 +41,3 @@ const AllProducts = () => {
 };
 
 export default AllProducts;
-
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -6,15 +6,18 @@ const Products = ({ product }) => {
   return (
     <div className="product">
       <img src={img} alt={name} className="product-img" />
-      <p className="text-xl font-semibold text-center mb-4">Name : {name}</p>
-      <p className="text-[16px] text-center mb-4">price : ${price}</p>
-      <p className="text-[16px] text-center mb-4">Type : ${type}</p>
-      <p className="text-[16px] text-center mb-4">Processor : ${processor}</p>
-      <p className="text-[16px] text-center mb-4">Memory : ${memory}</p>
-      <p className="text-[16px] text-center mb-4">Operating System : ${OS}</p>
-      <div className="btn-div">
-        {/* Add to Cart button with onClick handler */}
-        <button className="add-to-cart">Add to Cart</button>
+      <div className="mx-4 mt-5">
+        <p className="text-2xl font-bold mb-4">Name: {name}</p>
+        <p className="text-lg font-semibold mb-4">Price : {price}</p>
+        <p className="text-lg font-semibold mb-4">Type : {type}</p>
+        <p className="text-lg font-semibold mb-4">Processor : {processor}</p>
+        <p className="text-lg font-semibold mb-4">Memory : {memory}</p>
+        <p className="text-lg font-semibold mb-4">Operating System : {OS}</p>
+      </div>
+      <div className="mt-20">
+        <div className="btn-div">
+          <button className="add-to-cart">Add to Cart</button>
+        </div>
       </div>
     </div>
   );
